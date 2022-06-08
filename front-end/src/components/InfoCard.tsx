89ceee75964cf657b40877deb4082c618532d25b
Nam Nguyen <namnguyn3019@gmail.com>
2022-06-08 15:19:14 +0700

    Update infor about me when user log in

--- front-end/src/components/InfoCard.tsx
+++ front-end/src/components/InfoCard.tsx
@@ -2,14 +2,30 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import { useEffect } from 'react';
+import { useAppSelector } from '../state/hook';
 
 export default function InforCard() {
+    const user = useAppSelector(state => state.user)
+    const userEmail = user ? user.email : null
+
+    useEffect(() => {
+    }, [userEmail])
+
     return (
         <Card sx={{ minWidth: 200, textAlign: "center", mx: "auto" }}>
             <CardContent>
                 <p>About Me</p>
-                <p>My nam is Nam Nguyen</p>
-                <p> I am a software Engineer</p>
+                {
+                    userEmail ? (
+                        <>
+                            <p>My email address is: {userEmail}</p>
+                        </>
+                    ) : (
+                        <p>Login to write your first post</p>
+                    )
+                }
+
             </CardContent>
             <CardActions>
                 <Button size="small">Learn More</Button>
